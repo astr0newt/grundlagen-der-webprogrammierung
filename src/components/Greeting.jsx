76b76d24c057a42758d22c3347f581583b49a052
astr0newt <jakobpfefferkorn@posteo.de>
2023-02-02 15:40:51 +0100

    changed project to foodblog - added hero & recipes

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,9 +8,16 @@ export default function Greeting({ messages }) {
   const [greeting, setGreeting] = useState(randomMessage());
 
   return (
-    <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+    <div class="mb-8">
+      <h1 class="text-2xl text-center">
+        {greeting} visitor! Thank you for visiting!
+      </h1>
+      <button
+        class="h-10 w-32 text-base outline outline-2 outline-amber-200 p-1.5 hover:bg-amber-100 rounded-lg"
+        onClick={() => setGreeting(randomMessage())}
+      >
+        New Greeting
+      </button>
     </div>
   );
 }
