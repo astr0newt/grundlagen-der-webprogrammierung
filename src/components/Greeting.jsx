35af0e72f6ae8be1c8e9565ff891e0410d084590
astr0newt <jakobpfefferkorn@posteo.de>
2023-02-06 12:55:25 +0100

    revise footer and add flexbox

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,10 +8,8 @@ export default function Greeting({ messages }) {
   const [greeting, setGreeting] = useState(randomMessage());
 
   return (
-    <div class="mb-8">
-      <h1 class="text-2xl text-center">
-        {greeting} visitor! Thank you for visiting!
-      </h1>
+    <div class="mb-8 text-center">
+      <h1 class="text-2xl">{greeting} visitor! Thank you for visiting!</h1>
       <button
         class="h-10 w-32 text-base outline outline-2 outline-amber-200 p-1.5 hover:bg-amber-100 rounded-lg"
         onClick={() => setGreeting(randomMessage())}
