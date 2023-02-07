346247c471187404c4b94abcba6702aef7e20e36
astr0newt <jakobpfefferkorn@posteo.de>
2023-02-07 10:18:58 +0100

    complete first version and delete old files

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,8 +8,9 @@ export default function Greeting({ messages }) {
   const [greeting, setGreeting] = useState(randomMessage());
 
   return (
-    <div class="mb-8 text-center">
-      <h1 class="text-2xl">{greeting} visitor! Thank you for visiting!</h1>
+    <div class="mb-12 text-center">
+      <h1 class="text-2xl mb-2">{greeting}!</h1>
+      <h2 class="text-lg mb-4">Danke für deinen Besuch!</h2>
       <button
         class="h-10 w-32 text-base outline outline-2 outline-amber-200 p-1.5 hover:bg-amber-100 rounded-lg"
         onClick={() => setGreeting(randomMessage())}
